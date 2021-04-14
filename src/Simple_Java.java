219467be3b62b132bab323123181bcd60c1ed473
henry <2812483751@qq.com>
2021-04-14 19:27:35 +0800

    1

---
+++ src/Simple_Java.java
@@ -0,0 +1,23 @@
+int size = 27;
+String name = "Fido";
+Dog myDog = new DOg(name, size);
+x = size - 5;
+if (x < 15) myDog.bark(8);
+
+while (x > 3) {
+    myDog.play();
+}
+
+
+int[] numList = {2,4,6,8};
+System.oout.print("Hello");
+System.out.print("Dog: " + name);
+String num = "8";
+int z = Integer.parseInt(num);
+
+try {
+    readTheFile("myFile.txt");
+}
+catch(FileNotFoundException ex) {
+    System.out.print("File not found.");
+}
