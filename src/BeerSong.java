29c7c22d0333bd95c153d47b5699f0aaa3c98779
henry <2812483751@qq.com>
2021-04-14 19:53:19 +0800

    4-14第一晚

---
+++ src/BeerSong.java
@@ -0,0 +1,25 @@
+public class BeerSong {
+    public static void main (String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+
+            if (beerNum == 1) {
+                word = "bottle"; //单数的瓶子
+            }
+
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerNum = beerNum - 1;
+
+            if (beerNum > 0) {
+                System.out.println(beerNum + " " + word + " of beer on the wall");
+            } else {
+                System.out.println("No more bottles of beer on the wall");
+            } //else结束
+        } //while循环结束
+    } //main方法结束
+} //class结束
