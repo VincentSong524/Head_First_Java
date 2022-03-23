b7d39ea1c13ca091d382c912189ccfa3938e4721
Songwansen <2812483751@qq.com>
2022-03-23 16:48:31 +0800

    over dog

--- src/Dog.java
+++ src/Dog.java
@@ -3,7 +3,7 @@ class Dogs {
     String name;
 
     void bark() {
-        System.out.println("wang wang");
+        System.out.println(name + " says Ruff!");
     }
 
     void chaseCat() {
@@ -15,14 +15,31 @@ class Dogs {
 class Dog{
     public static void main(String[] args) {
         
-        //
-        Dogs[] myDogs = new Dogs[3];
-        myDogs[0] = new Dogs();
-        myDogs[0].name = "Fido";
-        myDogs[0].bark();
-        myDogs[0].chaseCat();
+        //创建Dog对象
+        Dogs dog1 = new Dogs();
+        dog1.bark();
+        dog1.name = "Bart";
 
+        //创建Dog数组
+        Dogs[] myDogs = new Dogs[3];
+        //关门放狗
+        myDogs[0] = new Dogs(); 
         myDogs[1] = new Dogs();
+        myDogs[2] = dog1;
+
+        //通过数组引用存取Dog
+        myDogs[0].name = "Fred";
         myDogs[1].name = "Marge";
+
+        //myog[2] 的名字是？
+        System.out.print("last dog's name is ");
+        System.out.println(myDogs[2].name);
+
+        //对Dog执行bark()
+        int x = 0;
+        while(x < myDogs.length) {
+            myDogs[x].bark();
+            x = x + 1;
+        }
     }
 }
