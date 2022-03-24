a1ef9dd18a37ba189a95529a81d8a5452b2d8cbe
Songwansen <2812483751@qq.com>
2022-03-24 20:49:19 +0800

    更改代码顺序

--- src/TestArrays.java
+++ src/TestArrays.java
@@ -1,20 +1,19 @@
 public class TestArrays {
     public static void main(String[] args) {
         
-        int y = 0;
         int [] index = new int[4];
-        String [] islands = new String[4];
-
         index[0] = 1;
         index[1] = 3;
         index[2] = 0;
         index[3] = 2;
 
+        String [] islands = new String[4];
         islands[0] = "Bermuda";
         islands[1] = "Fiji";
         islands[2] = "Azores";
         islands[3] = "Cozumel";
 
+        int y = 0;
         int ref;
         while ( y < 4) {
 
