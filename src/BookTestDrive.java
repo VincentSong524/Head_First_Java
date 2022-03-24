1044caf375a885ec56f5df2a0f66bd84308bffbc
Songwansen <2812483751@qq.com>
2022-03-24 20:54:50 +0800

    修改代码顺序，无功能改变

--- src/BookTestDrive.java
+++ src/BookTestDrive.java
@@ -8,12 +8,12 @@ public class BookTestDrive {
     public static void main(String[] args) {
         
         Books [] myBooks = new Books[3];
+        int x = 0;
 
         myBooks[0] = new Books();
         myBooks[1] = new Books();
         myBooks[2] = new Books();
 
-        int x = 0;
         myBooks[0].title = "The Grapes of Java";
         myBooks[1].title = "The Java Gatsby";
         myBooks[2].title = "The Java Cookbook";
