ff42508e767dd6b3076accf70e7b6ef99ff5fa83
Songwansen <2812483751@qq.com>
2022-03-24 17:28:18 +0800

    排排看练习

---
+++ src/TestSArrays.java
@@ -0,0 +1,30 @@
+public class TestSArrays {
+    public static void main(String[] args) {
+        
+        int y = 0;
+        int [] index = new int[4];
+        String [] islands = new String[4];
+
+        index[0] = 1;
+        index[1] = 3;
+        index[2] = 0;
+        index[3] = 2;
+
+        islands[0] = "Bermuda";
+        islands[1] = "Fiji";
+        islands[2] = "Azores";
+        islands[3] = "Cozumel";
+
+        int ref;
+        while ( y < 4) {
+
+            ref = index[y];
+
+            System.out.print("island = ");
+            System.out.println(islands[ref]);
+
+            y = y + 1;
+        }
+    }
+    
+}
