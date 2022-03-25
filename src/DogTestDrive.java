4807ec43ecec33da1a851ff11cd8992967c849d4
Songwansen <2812483751@qq.com>
2022-03-25 10:23:33 +0800

    通过判断执行不同的方法

---
+++ src/DogTestDrive.java
@@ -0,0 +1,31 @@
+class Dog_Bark {
+    int size;
+    String name;
+
+    void bark() {
+        if (size > 60) {
+            System.out.println("Wooof! Wooof!");
+        } else if (size > 14) {
+            System.out.println("Ruff! Ruff!");
+        } else  {
+            System.out.println("Yip! Yip!");
+        }
+    }
+}
+
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog_Bark one = new Dog_Bark();
+        one.size = 70;
+        Dog_Bark two = new Dog_Bark();
+        two.size = 8;
+        Dog_Bark three = new Dog_Bark();
+        three.size = 35;
+
+        one.bark();
+        two.bark();
+        three.bark();
+    }
+    
+}
