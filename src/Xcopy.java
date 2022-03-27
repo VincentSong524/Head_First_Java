8c60b2a464dad1e10fdbdb416ee672edb64d8b6e
Songwansen <2812483751@qq.com>
2022-03-27 10:39:28 +0800

    新增Xcopy

---
+++ src/Xcopy.java
@@ -0,0 +1,14 @@
+public class Xcopy {
+    public static void main(String[] args) {
+        int orig = 42;
+        Xcopy x = new Xcopy();
+        int y = x.go(orig);
+        System.out.println(orig + " " + y);
+    }
+
+    int go(int arg) {
+        arg = arg * 2;
+        return arg;
+    }
+    
+}
