c99c6b332829c7f1ecdafc7a2c8771e9d5f982e1
henry <2812483751@qq.com>
2022-03-28 22:03:56 +0800

    test back

--- src/test.java
+++ src/test.java
@@ -1,51 +1,11 @@
-class testDog {
-    private int size;
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int s) {
-        size = s;
-    }
-}
-
-class Test1 {
-    int var;
-
-    public int Test2(int c) {
-        if (var > 100) {
-            return var;
-        } else {
-            return c;
+public class Test {
+    public static void main(String[] args) {
+        String [] numArray = new String[10];
+        for ( int i = 0; i < 10; i++) {
+            numArray[i] = "a" + i;
+        }
+        for (String num: numArray) {
+            System.out.println(num);
         }
     }
-}
-class test {
-    public static void main(String[] args) {
-
-        testDog []  pets;
-        int p;
-        short c = 42;
-        pets = new testDog[7];
-        pets[0] = new testDog();
-        pets[1] = new testDog();
-        pets[0].setSize(30);
-        int x = pets[0].getSize();
-        pets[1].setSize(8);
-        System.out.println(x);
-
-        // 使用equals() 判断两个对象是否意义上相等
-       if (!pets[0].equals(pets[1])) {
-           System.out.println("False" + c);
-       }
-
-       pets[1].getSize();
-       Test1 test = new Test1();
-        p = test.Test2(101);
-        System.out.println(p);
-    }
-
-    
-    
 }
