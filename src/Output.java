060e972bcda85250e7f445e958d8ce1ef7128118
Songwansen <2812483751@qq.com>
2022-03-28 16:56:54 +0800

    添加Output

---
+++ src/Output.java
@@ -0,0 +1,21 @@
+public class Output {
+    public static void main(String[] args) {
+        Output o = new Output();
+        o.go();
+    }
+
+    void go() {
+        int y = 7;
+        for (int x = 1; x < 8; x++) { // 此处x++ 等于 x = x + 1
+            y++;
+            if (x > 4) {
+                System.out.print(++y + " ");
+            }
+            if (y > 14) {
+                System.out.print(" x = " + x);
+                break;
+            }
+        }
+    }
+    
+}
