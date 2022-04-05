7165f471220a9810c5b8afc6bb0fb3b730585de2
Songwansen <2812483751@qq.com>
2022-04-05 08:46:09 +0800

    测试Animal通用list

---
+++ src/Animal/AnimalTestDrive.java
@@ -0,0 +1,11 @@
+package Animal;
+
+public class AnimalTestDrive {
+    public static void main(String[] args) {
+        MyAnimalList list= new MyAnimalList();
+        Dog a= new Dog();
+        Cat c = new Cat();
+        list.add(a);
+        list.add(c);
+    }
+}
