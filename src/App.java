efbf31b6c2f9ff992dcf0d8bb4906abbfdc18b14
Songwasen <2812483751@qq.com>
2022-04-21 11:38:45 +0800

    code- server

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        System.out.println("Hello, 《Head_First_Java》!");
+        System.out.println("Hello, 《Head_First_Java》!"); 
+        System.ou.println("时间2022.04.21 code- server设置成功")
     }
 }
