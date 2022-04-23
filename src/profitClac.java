ae3fe2e46398eeb499a975393e7f3d051ad2fb65
Songwasen <2812483751@qq.com>
2022-04-23 10:58:53 +0800

    完善记账

--- src/profitClac.java
+++ src/profitClac.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
 
-import javax.swing.Box;
-import javax.swing.text.BoxView;
-
 class clac {
     
     /*
@@ -47,39 +44,39 @@ public class profitClac {
 
         //float fare = 88, boxInPrice = 100, commission = 100, profit, numberOfAccounts = 0, OtherNumberAccounts = 0;
         
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("输入参数\n盲盒出售价格: ");
-        float boxOutPrice = sc.nextFloat(); 
-        
-        //System.out.println("车费：");
-        //float fare = sc.nextFloat(); 
-        float fare = 88;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("输入参数\n盲盒出售价格: ");
+            float boxOutPrice = sc.nextFloat(); 
+            
+            //System.out.println("车费：");
+            //float fare = sc.nextFloat(); 
+            float fare = 88;
 
-        //System.out.println("盲盒购入价格: ");
-        //float boxInPrice = sc.nextFloat(); 
-        float boxInPrice = 100;
+            //System.out.println("盲盒购入价格: ");
+            //float boxInPrice = sc.nextFloat(); 
+            float boxInPrice = 100;
 
-        //System.out.println("非本号佣金: ");
-        //float commission = sc.nextFloat(); 
-        float commission = 100;
+            //System.out.println("非本号佣金: ");
+            //float commission = sc.nextFloat(); 
+            float commission = 100;
 
-        System.out.println("账号数量: ");
-        float numberOfAccounts = sc.nextFloat(); 
-        
-        System.out.println("非本号数量: ");
-        float OtherNumberAccounts = sc.nextFloat(); 
-        
-        System.out.println("碎片回收价格/个");
-        float reclaim = sc.nextFloat(); 
+            System.out.println("账号数量: ");
+            float numberOfAccounts = sc.nextFloat(); 
+            
+            System.out.println("非本号数量: ");
+            float OtherNumberAccounts = sc.nextFloat(); 
+            
+            System.out.println("碎片回收价格/个");
+            float reclaim = sc.nextFloat(); 
 
-        // clac.allProfit(boxOutPrice, fare, boxInPrice, commission, profit, numberOfAccounts, OtherNumberAccounts)
-        System.out.println("本人单号利润 =" + clac.sigleProfit(boxOutPrice, fare, boxInPrice, commission, numberOfAccounts, OtherNumberAccounts, reclaim) + 
-            " 非本人单号利润 =" +(clac.sigleProfit(boxOutPrice, fare, boxInPrice, commission, numberOfAccounts, OtherNumberAccounts, reclaim) - 100));
+            // clac.allProfit(boxOutPrice, fare, boxInPrice, commission, profit, numberOfAccounts, OtherNumberAccounts)
+            System.out.println("本人单号利润 =" + clac.sigleProfit(boxOutPrice, fare, boxInPrice, commission, numberOfAccounts, OtherNumberAccounts, reclaim) + 
+                " 非本人单号利润 =" +(clac.sigleProfit(boxOutPrice, fare, boxInPrice, commission, numberOfAccounts, OtherNumberAccounts, reclaim) - 100));
 
-        System.out.println("最终利润 =" + clac.allProfit(boxOutPrice, fare, boxInPrice, commission, numberOfAccounts, OtherNumberAccounts, reclaim));
+            System.out.println("最终利润 =" + clac.allProfit(boxOutPrice, fare, boxInPrice, commission, numberOfAccounts, OtherNumberAccounts, reclaim));
 
-        System.out.println("丢失一个账号减少收入为: " + clac.lossAccounts(boxOutPrice, fare, boxInPrice, commission));
+            System.out.println("丢失一个账号减少收入为: " + clac.lossAccounts(boxOutPrice, fare, boxInPrice, commission));
+        }
     }
     
 }
